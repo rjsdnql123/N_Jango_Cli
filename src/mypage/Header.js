04cb9b982e92a0131cab2727256b6cf0e6a4ec04
aiora32 <uukel32@gmail.com>
2020-05-25 17:48:59 +0900

    mypage, header

---
+++ src/mypage/Header.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line quotes
+import React from "react";
+// eslint-disable-next-line quotes
+import { Link } from "react-router-dom";
+
+export default () => (
+  <header className="header">
+    <ul className="ul">
+      <li className="li">
+        <Link to="/" className="link">
+          내 냉장고
+        </Link>
+      </li>
+      <li className="li">
+        <Link to="./Recipe" className="link">
+          내 레시피
+        </Link>
+      </li>
+      <li className="li">
+        <Link to="./Like" className="link">
+          좋아요
+        </Link>
+      </li>
+    </ul>
+  </header>
+);
