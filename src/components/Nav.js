de79c42d8a628f1bb5e692a43ecdfb4d36705410
Kjoobin <imkj1148@gmail.com>
2020-05-27 01:35:28 +0900

    Modify nav rayout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import './Nav.css';
+
 const Nav = ({ isLogin }) => (
   <div className="nav">
     {isLogin ? (
