b7c9dd745ea1047c9c7c0598e002748c35bc8c87
Kjoobin <imkj1148@gmail.com>
2020-05-27 13:31:34 +0900

    Modify redirect /mypage

--- src/pages/mypage/Header.js
+++ src/pages/mypage/Header.js
@@ -1,26 +1,44 @@
 // eslint-disable-next-line quotes
+/* eslint-disable */
 import React from 'react';
 // eslint-disable-next-line quotes
 import { Link } from 'react-router-dom';
+import './mypageHeader.css';
 
-export default () => (
-  <header className="header">
-    <ul className="ul">
-      <li className="li">
-        <Link to="/mypage" className="link">
-          내 냉장고
-        </Link>
-      </li>
-      <li className="li">
-        <Link to="/mypage/recipe" className="link">
-          내 레시피
-        </Link>
-      </li>
-      <li className="li">
-        <Link to="./Like" className="link">
-          좋아요
-        </Link>
-      </li>
-    </ul>
-  </header>
+const MyHeader = () => (
+  <div>
+    <center>
+      <div className="mypageheader">
+        <ul className="ul">
+          <li className="li">
+            <Link to="/mypage/" className="link">
+              내 냉장고
+            </Link>
+          </li>
+          <li className="li">
+            <Link to="/mypage/recipe" className="link">
+              내 레시피
+            </Link>
+          </li>
+          <li className="li">
+            <Link to="/mypage/like" className="link">
+              좋아요
+            </Link>
+          </li>
+          <li className="li">
+            <Link to="/mypage/follow" className="link">
+              팔로우
+            </Link>
+          </li>
+          <li className="li">
+            <Link to="/mypage/comment" className="link">
+              내 댓글
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </center>
+  </div>
 );
+
+export default MyHeader;
